a5a3ef9df6289cb4c740cedfba18cbe8f65b65b6
brooke <bhilbran@asu.edu>
2023-09-07 12:01:25 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(records) {
+    console.log(records)
+        
+    let superbowlWin = records.find(record => record.result === "W");
+  if (superbowlWin) {
+    return superbowlWin.year;
+  } else {
+    return (undefined)
+  }
+}
+    
